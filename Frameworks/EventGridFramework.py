adc3ae1bd28553f2d53658227bedd6e430051fc4
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2019-05-21 18:33:58 -0400

    Event Grid fixes

--- Frameworks/EventGridFramework.py
+++ Frameworks/EventGridFramework.py
@@ -6,15 +6,15 @@ import Frameworks.CommonFramework as CommonFramework
 import hashlib as hashlib
 
 async def publish_event(eventid,subject,data,eventtype):
-        config = CommonFramework.RetrieveConfigOptions("eventgrid")
-        credentials = TopicCredentials(config['key'])
-        event_grid_client = EventGridClient(credentials)
-        events=[{
-            'id': str(eventid),
-            'subject' : str(subject),
-            'data': [data],
-            'event_time': datetime.now(timezone.utc),
-            'event_type': str(eventtype),
-            'data_version': 1
-            }]    
-        event_grid_client.publish_events(config['hostname'],events)
+    config = CommonFramework.RetrieveConfigOptions("eventgrid")
+    credentials = TopicCredentials(config['key'])
+    event_grid_client = EventGridClient(credentials)
+    events=[{
+        'id': str(eventid),
+        'subject' : str(subject),
+        'data': [data],
+        'event_time': datetime.now(timezone.utc),
+        'event_type': str(eventtype),
+        'data_version': 1
+        }]    
+    event_grid_client.publish_events(config['hostname'],events)
