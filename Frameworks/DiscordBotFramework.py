ae9623f2aedda6a1fa577da278ab6d46504dfca7
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2019-06-24 23:43:13 -0400

    Registration code

--- Frameworks/DiscordBotFramework.py
+++ Frameworks/DiscordBotFramework.py
@@ -1,13 +1,26 @@
 import Frameworks.CosmosFramework as CosmosFramework
 import Frameworks.DiscordFramework as DiscordFramework
+import hashlib
+import datetime
 
 #Public def
 async def register(message: object) -> dict:
-    """Registration Information"""
+    """Registration"""
+    def genToken(discordId):
+        time = str(datetime.datetime.utcnow().microsecond) # only microseconds in last second. i.e. <100
+        hasher = hashlib.sha256(discordId.encode('utf-8'))
+        hasher.update(time.encode('utf-8'))
+        return hasher.hexdigest()[-8:]
+    
+    authordiscordid = message.author.id
     returnmessage = dict()
-    result = CosmosFramework.QueryItems('SELECT * FROM c WHERE c.discordid = "{0}"'.format(message.author.id))
+    result = CosmosFramework.QueryItems('SELECT * FROM c WHERE c.discordid = "{0}"'.format(authordiscordid))
     result = result[0]
-    if result['wgid'] is not None:
+    if bool(result):
+        document = dict()
+        document['discordid'] = authordiscordid
+        document['wgtoken'] = str(genToken(authordiscordid))
+    elif result['wgid'] is not None:
         returnmessage['private'] = "You have already registered"
     elif result['token'] is not None:
         returnmessage['private'] = ""
