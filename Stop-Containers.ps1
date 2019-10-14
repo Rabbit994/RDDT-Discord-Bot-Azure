6c9d9afe599b518237024fa6c43fec68ccd04504
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2019-10-14 17:39:26 -0400

    Adding No Window and params to Deploy

--- Stop-Containers.ps1
+++ Stop-Containers.ps1
@@ -1,6 +1,6 @@
-Start-Process 'docker' -ArgumentList "stop drblistener"
-Start-Process 'docker' -ArgumentList "stop drbhandler"
-Start-Process 'docker' -ArgumentList "stop drbconeremover"
-Start-Process 'docker' -ArgumentList "stop drbwotupdater"
-Start-Process 'docker' -ArgumentList "stop drbflasksite"
+Start-Process 'docker' -NoNewWindow -ArgumentList "stop drblistener"
+Start-Process 'docker' -NoNewWindow -ArgumentList "stop drbhandler"
+Start-Process 'docker' -NoNewWindow -ArgumentList "stop drbconeremover"
+Start-Process 'docker' -NoNewWindow -ArgumentList "stop drbwotupdater"
+Start-Process 'docker' -NoNewWindow -ArgumentList "stop drbflasksite"
 Start-Sleep 15 #Let the Docker Containers finishing stopping
