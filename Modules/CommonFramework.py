962aeb0100eb3db63decbc985fe03c96bf3a7c87
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2020-02-13 20:31:33 -0500

    fixing indentation bug

--- Modules/CommonFramework.py
+++ Modules/CommonFramework.py
@@ -30,17 +30,17 @@ def convert_date_time_epoch(datetime:str) -> int:
 
 def GetClanBattles(clanid):
     def clantools_get_json_data(uri: str) -> dict:
-    """Returns JSON Data in dict format"""
-    try:
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
-        response = urllib.request.urlopen(uri, timeout=60, context=ctx)
-        urldata = response.read().decode("utf-8","ignore")
-        jsondata = json.loads(urldata)
-        return jsondata
-    except:
-        return None
+        """Returns JSON Data in dict format"""
+        try:
+            ctx = ssl.create_default_context()
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+            response = urllib.request.urlopen(uri, timeout=60, context=ctx)
+            urldata = response.read().decode("utf-8","ignore")
+            jsondata = json.loads(urldata)
+            return jsondata
+        except:
+            return None
     
     clantoolsurl = 'https://sv.clantools.us/integrations/battles/report?provider=na&tz=et&service=slack&clan_id={0}'.format(clanid)
     data = clantools_get_json_data(clantoolsurl)
